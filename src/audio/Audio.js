c76f850a5320376e121b2252dabe3f0c72522905
milosdespotovic <91353969+milosdespotovic@users.noreply.github.com>
2023-02-19 12:17:06 -0800

    Install react-media-recorder

--- src/audio/Audio.js
+++ src/audio/Audio.js
@@ -1,3 +1,19 @@
+import { useReactMediaRecorder } from 'react-media-recorder';
+
 export default function Audio() {
-  return <div>Audio component</div>;
+  const onStopClicked = (blobUrl, blobData) => {
+    console.log('stop clicked', blobData);
+  };
+
+  const { status, startRecording, stopRecording, mediaBlobUrl } =
+    useReactMediaRecorder({ audio: true, onStop: onStopClicked });
+
+  return (
+    <div>
+      <p>{status}</p>
+      <button onClick={startRecording}>Start Recording</button>
+      <button onClick={stopRecording}>Stop Recording</button>
+      <audio src={mediaBlobUrl} controls />
+    </div>
+  );
 }

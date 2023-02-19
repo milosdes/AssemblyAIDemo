6d973bbb2877c65e9e57329557284073efd1333b
milosdespotovic <91353969+milosdespotovic@users.noreply.github.com>
2023-02-19 12:59:02 -0800

    Add style and functionality for audio component

--- src/audio/Audio.js
+++ src/audio/Audio.js
@@ -1,6 +1,9 @@
+import { useEffect, useState } from 'react';
 import { useReactMediaRecorder } from 'react-media-recorder';
+import { micIcon, micIconStyle } from '../icons';
 
 export default function Audio() {
+  const [isRecording, setIsRecording] = useState(false);
   const onStopClicked = (blobUrl, blobData) => {
     console.log('stop clicked', blobData);
   };
@@ -8,12 +11,31 @@ export default function Audio() {
   const { status, startRecording, stopRecording, mediaBlobUrl } =
     useReactMediaRecorder({ audio: true, onStop: onStopClicked });
 
+  let micContainerColor = isRecording ? 'bg-red-700' : 'bg-green-700';
+  let micContainerStyle = `p-1 w-min h-auto m-auto rounded-md hover:cursor-pointer hover:opacity-80 ${micContainerColor} hover:`;
+
+  let onMicClick = () => {
+    setIsRecording((prev) => {
+      if (!prev) {
+        console.log('recording started');
+        startRecording();
+      } else {
+        console.log('recording stopped');
+        stopRecording();
+      }
+      return !prev;
+    });
+  };
+
   return (
     <div>
       <p>{status}</p>
       <button onClick={startRecording}>Start Recording</button>
       <button onClick={stopRecording}>Stop Recording</button>
       <audio src={mediaBlobUrl} controls />
+      <div className={`${micContainerStyle} `} onClick={onMicClick}>
+        {micIcon}
+      </div>
     </div>
   );
 }
--- src/icons.js
+++ src/icons.js
@@ -1,10 +1,11 @@
-<svg
+export const micIcon = (
+  <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
     strokeWidth={1.5}
     stroke="white"
-    className="w-6 h-6"
+    className="w-10 h-10"
   >
     <path
       id="mic-icon"
@@ -13,3 +14,4 @@
       d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z"
     />
   </svg>
+);

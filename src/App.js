51cfb84f9c259468818020540796d8f7bb644230
milosdespotovic <91353969+milosdespotovic@users.noreply.github.com>
2023-02-19 12:04:08 -0800

    Create options, audio, and display components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import Audio from './audio/Audio';
+import Display from './display/Display';
+import Options from './options/Options';
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <>
+      <Options />
+      <Audio />
+      <Display />
+    </>
+  );
 }
 
 export default App;
